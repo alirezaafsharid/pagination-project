edd350a92ce39efd3935e7830663d90de36ce4c1
alireza <afshar2451382@gmail.com>
2024-09-02 00:30:29 +0330

    end

--- pagination-project/src/App.js
+++ pagination-project/src/App.js
@@ -1,15 +1,52 @@
 import React, { useEffect, useState } from "react";
+import Pagination from "react-bootstrap/Pagination";
+import { BsCursor } from "react-icons/bs";
 
 export default function App() {
   const [todos, setTodos] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [paginatedTodos, setPaginatedTodos] = useState([]);
+  // const [isPending, setIsPending] = useState(true);
+
+  let pageSize = 10;
+  let pagesNumbers;
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/todos")
       .then((res) => res.json())
       .then((datas) => {
         setTodos(datas);
+        let endIndex = pageSize * currentPage;
+        let startIndex = endIndex - pageSize;
+        let allShownTodos = datas.slice(startIndex, endIndex);
+        setPaginatedTodos(allShownTodos);
       });
   }, []);
+
+  const pagesCount = Math.ceil(todos.length / pageSize);
+  pagesNumbers = Array.from(Array(pagesCount).keys());
+  // coment/////////////////////////////////////////////////////////////////////.......................................
+  // این تیکه که جدا کردم خیلی مهم هستش حتما بعدا بهش توجه کن
+  useEffect(() => {
+    let endIndex = pageSize * currentPage;
+    let startIndex = endIndex - pageSize;
+    let allShownTodos = todos.slice(startIndex, endIndex);
+    setPaginatedTodos(allShownTodos);
+  });
+  const changePaginate = (newPage) => {
+    setCurrentPage(newPage);
+  };
+  // coment/////////////////////////////////////////////////////////////////....................................
+
+  // let active = 2;
+  // let items = [];
+  // for (let number = 1; number <= 5; number++) {
+  //   items.push(
+  //     <Pagination.Item key={number} active={number === active}>
+  //       {number}
+  //     </Pagination.Item>
+  //   );
+  // }
 
   return (
     <div>
@@ -27,8 +64,8 @@ export default function App() {
           </thead>
 
           <tbody>
-            {todos.map((todo) => (
-              <tr>
+            {paginatedTodos.map((todo) => (
+              <tr key={todo.id}>
                 <td>{todo.id}</td>
                 <td>{todo.userId}</td>
                 <td>{todo.title}</td>
@@ -46,6 +83,26 @@ export default function App() {
           </tbody>
         </table>
       )}
+
+      <nav className="d-flex justify-content-center">
+        <ul className="pagination" aria-current="page">
+          {pagesNumbers.map((pageNumber) => (
+            <li
+              style={{ cursor: "pointer" }}
+              className={
+                pageNumber + 1 === currentPage
+                  ? "page-item  active"
+                  : "page-item"
+              }
+              id=""
+              key={pageNumber + 1}
+              onClick={() => changePaginate(pageNumber + 1)}
+            >
+              <span className="page-link">{pageNumber + 1}</span>
+            </li>
+          ))}
+        </ul>
+      </nav>
     </div>
   );
 }
